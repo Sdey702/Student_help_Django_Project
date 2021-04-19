381aab49c2e27682b3ae7ce3203f2cfc6307b17d
Sdey702 <subhankardey.kalirhat@gmail.com>
2021-04-19 10:10:05 +0530

    new commit

--- student_help/Blog/models.py
+++ student_help/Blog/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from comment.models import Comment
 from django.contrib.contenttypes.fields import GenericRelation
-
+from PIL import Image
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=100)
@@ -17,9 +17,29 @@ class BlogPost(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     comments = GenericRelation(Comment)
 
+
+ 
+
     def __str__(self):
         return self.title
     
+    def save(self, *args, **kwargs):
+        super(BlogPost, self).save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 800 or img.width > 2000:
+            output_size = (600, 700)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
+    class Meta:
+        ordering = ['-date_posted']
+
+    
+
+
+
 class AddPost(models.Model):
     title = models.CharField(max_length=100)
     intro = models.CharField(max_length=400)
